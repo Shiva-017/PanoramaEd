9730ea30b41fe3d4708ad5ea1f8ec3fef9fc0fed
AnthonyKodirekka <kodirekka.a@northeastern.edu>
2023-11-21 17:22:35 -0500

    Added api changes for Student v4

--- Server/app/routes/chat-routes.js
+++ Server/app/routes/chat-routes.js
@@ -3,7 +3,6 @@ import * as chatController from "../controllers/chat-controller.js";
 const router = express.Router();
 
 router.route('/')
-    .get(chatController.find)
     .post(chatController.post)
 
 export default router;
