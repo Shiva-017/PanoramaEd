9829078bb92fc83829b29b6a6abaf30452dd29a2
Sai Nikhil Reddy Challa <challa.sain@northeastern.edu>
2023-12-08 19:56:44 -0500

    Added consultantId to chat

--- Server/app/routes/chat-routes.js
+++ Server/app/routes/chat-routes.js
@@ -3,6 +3,7 @@ import * as chatController from "../controllers/chat-controller.js";
 const router = express.Router();
 
 router.route('/')
+    .get(chatController.show)
     .post(chatController.post)
 
 export default router;
