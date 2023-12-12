6c7effd2fe7d0758a3f6afdce9112562303775a1
Chalapati Rao Konujula <konujula.c@northeastern.edu>
2023-12-11 20:22:15 -0500

    added Redux implementation for StudentPosts

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import collegeSlice from "./slices/college-slice";
+import postSlice from "./slices/StudentPost-slice"
+
 
 export const store = configureStore({
     reducer:{
-        [collegeSlice.name]: collegeSlice.reducer
+        [collegeSlice.name]: collegeSlice.reducer,
+        [postSlice.name]: postSlice.reducer
+
     }
 });
 
